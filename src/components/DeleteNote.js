e7635b66e1f0a14d617c6ec287d92895a0307548
NatasaVasicRA10 <vasic.ra10.2018@uns.ac.rs>
2022-11-08 10:12:21 +0100

    edit note

--- src/components/DeleteNote.js
+++ src/components/DeleteNote.js
@@ -1,33 +1,82 @@
+import React, { useState } from 'react';
 import FormLabel from '@mui/material/FormLabel';
 import Grid from '@mui/material/Grid';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import PropTypes from 'prop-types';
+import EditIcon from '@mui/icons-material/Edit';
+import TextField from '@mui/material/TextField';
+import TextareaAutosize from '@mui/base/TextareaAutosize';
 
-const DeleteNote = ({handleDelete, id, title, text, date, noteColor}) => {
+const DeleteNote = ({handleDelete, id, title, text, date, noteColor, handleEdit}) => {
+
+  const [ edit, setEdit ]= useState(false);
+  const [ editTitle, setEditTitle ]= useState(title);
+  const [ editText, setEditText ]= useState(text);
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <FormLabel className='NoteTitle'>
-          {title}
-        </FormLabel>
-      </Grid>
-      <Grid item className='NoteHeight' xs={12}>
-        <FormLabel className='NoteText'>
-          {text}
-        </FormLabel>
-      </Grid>
-      <Grid item className='DatePosition' xs={6}>
-        <FormLabel className='NoteDate'>
-          {date}
-        </FormLabel>
-      </Grid>
-      <Grid item className='DeleteButtonPosition' xs={5}>
-        <button className='DeleteButton' style ={{backgroundColor: noteColor}} onClick={() => handleDelete(id)}>
-          <DeleteForeverIcon/>
-        </button>
-      </Grid>
-    </Grid>
+    <div>
+      {edit ? (
+        <Grid container spacing={2}>
+          <form onSubmit={(e) => handleEdit(e, id, editTitle, editText, date, noteColor) || setEdit(!edit)}>
+            <Grid container spacing={2}>
+              <Grid item className='AddNoteItem' xs={12}>
+                <TextField
+                  className='AddNoteTextField'
+                  defaultValue={editTitle}
+                  required={true}
+                  variant='standard'
+                  InputProps={{disableUnderline: true}}
+                  onChange={(e) => {
+                    setEditTitle(e.target.value);
+                  }}/>
+              </Grid>
+              <Grid item className='AddNoteItem' xs={12}>
+                <TextareaAutosize
+                  minRows={10}
+                  className='AddNoteTextArea'
+                  maxLength={300}
+                  required={true}
+                  defaultValue={editText}
+                  onChange={(e) => {
+                    setEditText(e.target.value);
+                  }}/>
+              </Grid>
+              <Grid item className='DeleteButtonPosition' xs={5}>
+                <button className='AddButton'>
+                  Edit
+                </button>
+              </Grid>
+            </Grid>
+          </form>
+        </Grid>
+      ) : (
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <FormLabel className='NoteTitle'>
+              {editTitle}
+            </FormLabel>
+          </Grid>
+          <Grid item className='NoteHeight' xs={12}>
+            <FormLabel className='NoteText'>
+              {editText}
+            </FormLabel>
+          </Grid>
+          <Grid item className='DatePosition' xs={6}>
+            <FormLabel className='NoteDate'>
+              {date}
+            </FormLabel>
+          </Grid>
+          <Grid item className='DeleteButtonPosition' xs={5}>
+            <button className='DeleteButton' style ={{backgroundColor: noteColor}} onClick={() => setEdit(!edit)}>
+              <EditIcon/>
+            </button>
+            <button className='DeleteButton' style ={{backgroundColor: noteColor}} onClick={() => handleDelete(id)}>
+              <DeleteForeverIcon/>
+            </button>
+          </Grid>
+        </Grid>
+      )}
+    </div>
   );
 
 };
@@ -40,5 +89,6 @@ DeleteNote.propTypes = {
   title: PropTypes.string,
   text: PropTypes.string,
   date: PropTypes.any,
-  noteColor: PropTypes.string
+  noteColor: PropTypes.string,
+  handleEdit: PropTypes.func
 };
